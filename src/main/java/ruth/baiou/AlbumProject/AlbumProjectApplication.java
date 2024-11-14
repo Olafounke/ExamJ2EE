d9eda65a73830c9e52a6dfe0d780bf7d6327db9f
ruth <olafounkebiaou@gmail.com>
2024-11-14 10:08:06 +0100

    Initialisation du projet

---
+++ src/main/java/ruth/baiou/AlbumProject/AlbumProjectApplication.java
@@ -0,0 +1,13 @@
+package ruth.baiou.AlbumProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruth/baiou/AlbumProject/AlbumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ruth.baiou.AlbumProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
